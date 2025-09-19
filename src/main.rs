d6807b1d8dbb61c12cb0beeff0e98914c20028cd
ktan-wolf <ktan247610@gmail.com>
2025-09-19 05:07:19 +0000

    after deployment configuration

--- src/main.rs
+++ src/main.rs
@@ -95,7 +95,7 @@ async fn main() -> Result<(), AppError> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "8081".to_string());
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await?;
     println!("\n🚀 API server listening on http://{}", listener.local_addr()?);
-    println!("   Try accessing http://localhost:8081/nodes in your browser.");
+    println!("   Try accessing https://indexer-o06a.onrender.com/nodes in your browser.");
     axum::serve(listener, app).await?;
 
     Ok(())
